60032b7a72e247256e3fd2118087bf7dab091339
Madhura Bhavana <madhurabhanu0777@gmail.com>
2025-06-25 20:09:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://BhavanaMadhura:BHANU000@cluster0.pbqrtut.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://BhavanaMadhura:BHANU000@cluster0.pbqrtut.mongodb.net/')
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server running on ${PORT}`);
 });
