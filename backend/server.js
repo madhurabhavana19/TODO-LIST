5741835f008190baeda90a624fd955f97bfe75ab
Madhura Bhavana <madhurabhanu0777@gmail.com>
2025-07-17 16:50:09 +0530

    Install dotenv

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('process.env.mongodb+srv://BhavanaMadhura:BHANU000@cluster0.pbqrtut.mongodb.net/todoDB?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
