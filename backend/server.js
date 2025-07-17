4092030ed081c0cb758135d3694b5704fe5d7d28
Madhura Bhavana <madhurabhanu0777@gmail.com>
2025-07-17 16:44:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,14 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('process.env.mongodb+srv://BhavanaMadhura:BHANU000@cluster0.pbqrtut.mongodb.net/')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Backend is live");
+});
 
-app.use('/api/todos', todoRoutes);
+app.use("/api/todos", todoRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0.0.0.0', () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
